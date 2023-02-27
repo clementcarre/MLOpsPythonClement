2018fd9f81f7140be5cfe082437158baaabe0812
Clement <clement.carre33@gmail.com>
2023-02-27 12:29:52 +0100

    séance du 27/02/2023

--- tests/extraction_test.py
+++ tests/extraction_test.py
@@ -1,11 +1,18 @@
 import unittest
+from pathlib import Path
 
-class TestStringMethods(unittest.TestCase):
+from extraction import extract_images_from_pdf
 
-    def test_upper(self):
-        value = 'foo'.upper()
-        expected = 'FOO'
-        self.assertEqual(value, expected)
+BASE_PATH = Path(__file__).resolve().parent
+output_directory = BASE_PATH / "output"
+input_directory = BASE_PATH / "input"
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        extract_images_from_pdf(str(input_directory), str(output_directory))
+        self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
--- train/extraction/extraction.py
+++ train/extraction/extraction.py
@@ -14,21 +14,22 @@ def convert_pixmap_to_rgb(pixmap) -> Pixmap:
 pdfs_directory_path = ".\dataset-cats-dogs-others"
 images_directory_path = ".\extracted_images"
 
-Path(images_directory_path).mkdir(parents=True, exist_ok=True)
-files = [p for p in Path(pdfs_directory_path).iterdir() if p.is_file()]
-for path in files:
-    with open(path, "rb") as str:
-        with fitz.open(stream=str.read(), filetype="pdf") as d:
-            file_images = []
-            nb = len(d) - 1
-            for i in range(nb):
-                page = d[i]
-                imgs = d.get_page_images(i)
-                nb_imgs = len(imgs)
-                for j, img in enumerate(imgs):
-                    xref = img[0]
-                    pix = fitz.Pixmap(d, xref)
-                    bytes = BytesIO(convert_pixmap_to_rgb(pix).tobytes())
-                    fn = path.stem + "_page" + str(i) + "_index" + str(j) + ".png"
-                    with open(images_directory_path + "\\" + fn, "wb") as f:
-                        f.write(bytes.getbuffer())
+def extract_images_from_pdf(pdfs_directory_path:str,images_directory_path:str):
+    Path(images_directory_path).mkdir(parents=True, exist_ok=True)
+    files = [p for p in Path(pdfs_directory_path).iterdir() if p.is_file()]
+    for path in files:
+        with open(path, "rb") as str:
+            with fitz.open(stream=str.read(), filetype="pdf") as d:
+                file_images = []
+                nb = len(d) - 1
+                for i in range(nb):
+                    page = d[i]
+                    imgs = d.get_page_images(i)
+                    nb_imgs = len(imgs)
+                    for j, img in enumerate(imgs):
+                        xref = img[0]
+                        pix = fitz.Pixmap(d, xref)
+                        bytes = BytesIO(convert_pixmap_to_rgb(pix).tobytes())
+                        fn = path.stem + "_page" + str(i) + "_index" + str(j) + ".png"
+                        with open(images_directory_path + "\\" + fn, "wb") as f:
+                            f.write(bytes.getbuffer())
